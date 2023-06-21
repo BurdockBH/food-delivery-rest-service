0605975269230347d58f2cec1de14037e3c55f0a
Burdock <edocicak@gmail.com>
2023-06-21 10:33:49 +0200

    Expanded tests to test every possible incorrect parameter

--- db/db_test.go
+++ db/db_test.go
@@ -6,26 +6,70 @@ import (
 )
 
 func TestValidateDbConfig(t *testing.T) {
-	const (
-		username = "root"
-		password = "root"
-		host     = "localhost"
-		port     = "3306"
-		dbname   = "food_delivery"
-	)
 
-	cfg := &config.DatabaseConfig{
-		DBUsername: username,
-		DBPassword: password,
-		DBHost:     host,
-		DBPort:     port,
-		DBName:     dbname,
+	testCases := []config.DatabaseConfig{
+		{
+			DBUsername: "",
+			DBPassword: "password",
+			DBHost:     "localhost",
+			DBPort:     "port",
+			DBName:     "dbname",
+		},
+		{
+			DBUsername: "user",
+			DBPassword: "",
+			DBHost:     "localhost",
+			DBPort:     "port",
+			DBName:     "dbname",
+		},
+		{
+			DBUsername: "user",
+			DBPassword: "password",
+			DBHost:     "",
+			DBPort:     "port",
+			DBName:     "dbname",
+		},
+		{
+			DBUsername: "user",
+			DBPassword: "password",
+			DBHost:     "localhost",
+			DBPort:     "",
+			DBName:     "dbname",
+		},
+		{
+			DBUsername: "user",
+			DBPassword: "password",
+			DBHost:     "localhost",
+			DBPort:     "port",
+			DBName:     "",
+		},
 	}
 
-	err := ValidateDbConfig(cfg)
+	correctConfig := config.DatabaseConfig{
+		DBUsername: "user",
+		DBPassword: "password",
+		DBHost:     "localhost",
+		DBPort:     "port",
+		DBName:     "dbname",
+	}
+
+	failed := false
+	for _, test := range testCases {
+		err := ValidateDbConfig(&test)
+		if err == nil {
+			t.Errorf("Config %v should have failed validation", test.DBName)
+			failed = true
+		}
+	}
+
+	err := ValidateDbConfig(&correctConfig)
 	if err != nil {
-		t.Errorf("got error: %v", err)
+		t.Errorf("Error on valid data: %v", correctConfig.DBName)
+	}
+
+	if failed {
 		return
 	}
-	t.Logf("config is valid")
+
+	t.Logf("Validation function is validating correctly")
 }
--- viewmodels/vieiwmodels_test.go
+++ viewmodels/vieiwmodels_test.go
@@ -6,7 +6,52 @@ import (
 )
 
 func TestUser_Validate(t *testing.T) {
-	var u = User{
+
+	var testCases = []User{
+		{
+			Name:      "Edo123",
+			Email:     "edocicak@gmail.com",
+			Password:  "password",
+			Phone:     "1234567890",
+			CreatedAt: time.Now().Unix(),
+			UpdatedAt: time.Now().Unix(),
+		},
+		{
+			Name:      "Burdock",
+			Email:     "edodafkoa.com",
+			Password:  "password",
+			Phone:     "1234567890",
+			CreatedAt: time.Now().Unix(),
+			UpdatedAt: time.Now().Unix(),
+		},
+		{
+			Name:      "Burdock",
+			Email:     "edocicak@gmail.com",
+			Password:  "pass",
+			Phone:     "1234567890",
+			CreatedAt: time.Now().Unix(),
+			UpdatedAt: time.Now().Unix(),
+		},
+		{
+			Name:      "Burdock",
+			Email:     "edocicak@gmail.com",
+			Password:  "password",
+			Phone:     "1234567abcd",
+			CreatedAt: time.Now().Unix(),
+			UpdatedAt: time.Now().Unix(),
+		},
+	}
+
+	failed := false
+	for _, u := range testCases {
+		err := u.Validate()
+		if err == nil {
+			t.Errorf("User: %v should have failed validation", u.Name)
+			failed = true
+		}
+	}
+
+	correctUser := User{
 		Name:      "Burdock",
 		Email:     "edocicak@gmail.com",
 		Password:  "password",
@@ -15,56 +60,140 @@ func TestUser_Validate(t *testing.T) {
 		UpdatedAt: time.Now().Unix(),
 	}
 
-	err := u.Validate()
+	err := correctUser.Validate()
 	if err != nil {
-		t.Error(err)
+		t.Errorf("Error on valid data: %v", correctUser.Name)
 		return
 	}
-	t.Logf("User: %v validated succesfully", u.Name)
+
+	if failed {
+		return
+	}
+
+	t.Log("User validation is validating correctly")
 }
 
 func TestUserLoginRequest_ValidateLogin(t *testing.T) {
-	var u = UserLoginRequest{
-		Email:    "edocicak@gmail.com",
-		Password: "password"}
+	testCases := []UserLoginRequest{
+		{
+			Email:    "edocicak.com",
+			Password: "password",
+		},
+		{
+			Email:    "edocicak@gmail.com",
+			Password: "passasdofjadsjfoiaj0erjt0j092j9i2",
+		},
+	}
 
-	err := u.ValidateLogin()
+	failed := false
+	for _, u := range testCases {
+		err := u.ValidateLogin()
+		if err == nil {
+			t.Errorf("User: %v should have failed validation", u.Email)
+			failed = true
+		}
+	}
+
+	correctUserLogin := UserLoginRequest{
+		Email:    "edocicak@gmail.com",
+		Password: "password123",
+	}
+
+	err := correctUserLogin.ValidateLogin()
 	if err != nil {
-		t.Error(err)
+		t.Errorf("Error on valid data: %v", correctUserLogin.Email)
 		return
 	}
-	t.Logf("User: %v validated succesfully", u.Email)
+
+	if failed {
+		return
+	}
+
+	t.Log("User login validation is validating correctly")
 }
 
 func TestProduct_ValidateProduct(t *testing.T) {
-	var p = Product{
-		Name:        "Burger",
-		Description: "Burger with fries",
-		Price:       10,
-		CreatedAt:   time.Now().Unix(),
-		UpdatedAt:   time.Now().Unix(),
+	testCases := []Product{
+		{
+			Name:        "",
+			Description: "A delicious burger",
+			Price:       5.99,
+		},
+		{
+			Name:        "Burger",
+			Description: "",
+			Price:       5.99,
+		},
+		{
+			Name:        "Burger",
+			Description: "A delicious burger",
+			Price:       -5.99,
+		},
 	}
 
-	err := p.ValidateProduct()
+	failed := false
+	for _, p := range testCases {
+		err := p.ValidateProduct()
+		if err == nil {
+			t.Errorf("Product: %v should have failed validation", p.Name)
+			failed = true
+		}
+	}
+
+	correctProduct := Product{
+		Name:        "Burger",
+		Description: "A delicious burger",
+		Price:       5.99,
+	}
+
+	err := correctProduct.ValidateProduct()
 	if err != nil {
-		t.Error(err)
+		t.Errorf("Error on valid data: %v", correctProduct.Name)
 		return
 	}
-	t.Logf("Product: %v validated succesfully", p.Name)
+
+	if failed {
+		return
+	}
+
+	t.Log("Product validation is validating correctly")
 }
 
 func TestFoodVenue_ValidateFoodVenue(t *testing.T) {
-	var fv = FoodVenue{
-		Name:      "Burger King",
-		Address:   "123 Main St",
-		CreatedAt: time.Now().Unix(),
-		UpdatedAt: time.Now().Unix(),
+	testCases := []FoodVenue{
+		{
+			Name:    "",
+			Address: "1234 Main St",
+		},
+		{
+			Name:    "Burger",
+			Address: "",
+		},
 	}
 
-	err := fv.ValidateFoodVenue()
+	failed := false
+	for _, fv := range testCases {
+		err := fv.ValidateFoodVenue()
+		if err == nil {
+			t.Errorf("FoodVenue: %v should have failed validation", fv.Name)
+			failed = true
+		}
+	}
+
+	correctFoodVenue := FoodVenue{
+		Name:    "Burger",
+		Address: "1234 Main St",
+	}
+
+	err := correctFoodVenue.ValidateFoodVenue()
 	if err != nil {
-		t.Error(err)
+		t.Errorf("Error on valid data: %v", correctFoodVenue.Name)
 		return
 	}
-	t.Logf("FoodVenue: %v validated succesfully", fv.Name)
+
+	if failed {
+		return
+	}
+
+	t.Log("FoodVenue validation is validating correctly")
 }
